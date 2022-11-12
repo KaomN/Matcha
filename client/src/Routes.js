4d82a6c10f6915688571705b2302a7dac2e46403
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-12 12:29:31 +0200

    Added Forgot password route and verification route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import ForgotPassword from "./containers/ForgotPassword";
 import NotFound from "./containers/NotFound";
 
 export default function Links() {
@@ -10,7 +11,9 @@ export default function Links() {
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/login" element={<Login />} />
+			<Route path="/verification/*" element={<Login />} />
 			<Route path="/signup" element={<Signup />} />
+			<Route path="/forgotpassword" element={<ForgotPassword />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
