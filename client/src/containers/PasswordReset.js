591b0ad7f474486b267ecbc3e613e1880bfbfdec
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-14 14:41:53 +0200

    added password reset view

---
+++ client/src/containers/PasswordReset.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import "./styles/PasswordReset.css";
+
+export default function NotFound() {
+
+	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
+
+	const [errorPassword, setErrorPassword] = useState("");
+	const [errorPasswordConfirm, setErrorPasswordConfirm] = useState("");
+	const [message, setMessage] = useState("");
+	const [messageType, setMessageType] = useState("");
+
+	const [searchParams] = useSearchParams();
+	//console.log(searchParams.get("token"))
+	async function handleSubmit(event) {
+		event.preventDefault();
+		console.log(searchParams.get("token"))
+		let response = await fetch('/request/passwordreset', {
+			method: "POST",
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({
+				password: password,
+				passwordConfirm: passwordConfirm,
+				token: searchParams.get("token"),
+			})
+		});
+		response = await response.json();
+		console.log(response);
+		if(response.status) {
+			setMessageType("form_message_success");
+			setMessage(response.message);
+		} else {
+			if ("error" in response) {
+				setMessageType("form_message_error");
+				setMessage(response.error);
+			} else {
+				setErrorPassword(response.errorPassword);
+				setErrorPasswordConfirm(response.errorPasswordConfirm);
+			}
+		}
+	}
+
+	return (
+		<main className="form-container">
+			<form onSubmit={handleSubmit}>
+				<div className="lock-image-container">
+					<i className="material-icons lock-reset">lock_reset</i>
+				</div>
+				<h3>Camagru Password Reset</h3>
+				<div className="center">
+					<div className={messageType}>{message}</div>
+				</div>
+				<div className="form_input_group">
+					<input autoFocus type="password" name="password" className="form_input" placeholder="New Password" autoComplete="off" value={password} onChange={function(e) {setPassword(e.target.value); setErrorPassword(""); setMessage("")}}/>
+					<div className="form_input_error_message">{errorPassword}</div>
+				</div>
+				<div className="form_input_group">
+					<input type="password" name="passwordConfirm" className="form_input" placeholder="Confirm Password" autoComplete="off" value={passwordConfirm} onChange={function(e) {setPasswordConfirm(e.target.value); setErrorPasswordConfirm(""); setMessage("")}}/>
+					<div className="form_input_error_message">{errorPasswordConfirm}</div>
+				</div>
+				<div className="button_container">
+					<button type="submit" className="form_button">Submit</button>
+				</div>
+			</form>
+		</main>
+	);
+}
