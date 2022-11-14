802b89c6b00ad1714f571882a1951cfb837dbc21
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-14 09:29:21 +0200

    added user verification

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import "./Home.css";
 
 export default function Home() {
+	const [popup, setPopup] = useState("");
+	const [searchParams, setSearchParams] = useSearchParams();
+	useEffect(() => {
+		if(searchParams.get("verification") !== null){
+			async function verifyUser(token) {
+				let response = await fetch('/request/verify', {
+					method: "POST",
+					headers: { 'content-type': 'application/json' },
+					body: JSON.stringify({
+						token: token,
+					})
+				});
+				response = await response.json();
+				if(response.status === true && !response.verified) {
+					setPopup(	<div className="popup">
+									<div className="popup-content">
+										<p>Success! Your account is now verified!</p>
+										<div className="center">
+											<a href="/login"><button type="button" className="form_button_verify">Login!</button></a>
+										</div>
+									</div>
+								</div>);
+				} else if(!response.status) {
+					setPopup(	<div className="popup">
+									<div className="popup-content">
+										<p>Please follow the link you received on your email to verify your account!</p>
+										<div className="center">
+											<button type="button" className="form_button_verify" onClick={function(e) {setPopup("")}}>Close</button>
+										</div>
+									</div>
+								</div>);
+				}
+			}
+			verifyUser(searchParams.get("verification"));
+		}
+	}, []);
 	return (
-		<div className="home">
+		<main>
+			<div>
 				<img src="images/logo.png" alt="logo"></img>
-		</div>
+			</div>
+			{popup}
+		</main>
 	);
 }
