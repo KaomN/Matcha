80a2d714677d6523f48f0add1bca8b8a7c476a50
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-18 21:13:06 +0200

    modified database creation

--- server/createDatabase.js
+++ server/createDatabase.js
@@ -5,7 +5,7 @@ function createDatabase() {
 	const con = require("./setup").con;
 	con.query("CREATE DATABASE IF NOT EXISTS matcha");
 	con.changeUser({database: "matcha"});
-	con.query("CREATE TABLE IF NOT EXISTS users (pk_userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20) BINARY NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(50) NOT NULL, surname VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL, verified BOOLEAN NOT NULL DEFAULT FALSE, token varchar(70) DEFAULT NULL, gender VARCHAR(10) DEFAULT NULL, age INT DEFAULT NULL, genderpreference VARCHAR(20) DEFAULT NULL, biography TEXT DEFAULT NULL, rating INT DEFAULT 0)");
+	con.query("CREATE TABLE IF NOT EXISTS users (pk_userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20) BINARY NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(50) NOT NULL, surname VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL, verified BOOLEAN NOT NULL DEFAULT FALSE, token varchar(70) DEFAULT NULL, gender VARCHAR(10) DEFAULT NULL, age INT DEFAULT NULL, genderpreference VARCHAR(20) DEFAULT NULL, biography TEXT DEFAULT NULL, rating INT DEFAULT 0, profile BOOLEAN NOT NULL DEFAULT FALSE)");
 	con.query("CREATE TABLE IF NOT EXISTS images (imageid INT NOT NULL PRIMARY KEY, profilepic INT NOT NULL, fk_userid INT)");
 	con.query("CREATE TABLE IF NOT EXISTS tagitem (fk_userid INT NOT NULL, fk_tagid INT NOT NULL)");
 	con.query("CREATE TABLE IF NOT EXISTS tag (pk_tagid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, tag VARCHAR(25))");
