2764d0ec178d526e43eefd2b809f9b85b4893d50
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-23 10:47:48 +0200

    adding views

---
+++ client/src/containers/Chat.js
@@ -0,0 +1,10 @@
+import {useState} from "react";
+import "./styles/Home.css";
+
+export default function Chat() {
+	return (
+		<main>
+			<h3>Chat page. Show connected users on a panel to the left. clicking on the user brings up the Chat page in the middle with history of the chat</h3>
+		</main>
+	);
+}
---
+++ client/src/containers/Profile.js
@@ -0,0 +1,10 @@
+import {useState} from "react";
+import "./styles/Home.css";
+
+export default function Profile() {
+	return (
+		<main>
+			<h3>Profile Pages. Show profile page of user depending of the URL</h3>
+		</main>
+	);
+}
