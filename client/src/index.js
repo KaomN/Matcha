c513342bfd1abf2f650fe6f64815b9769e6dfc68
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-28 11:53:27 +0200

    moved Promise spinner to index

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import { UserProvider } from './components/UserContext';
+import { LoadingSpinnerPromise } from './components/LoadingSpinnerPromise';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Router>
 		<UserProvider>
 			<App />
+			<LoadingSpinnerPromise />
 		</UserProvider>
 	</Router>
 );
