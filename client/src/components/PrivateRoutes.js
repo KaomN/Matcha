d53a8bc8fa2913dfb0866c457b211446c1b8dea9
Cong Nguyen <kaom.n.92@gmail.com>
2022-11-28 11:50:19 +0200

    Added Private routes

---
+++ client/src/components/PrivateRoutes.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState} from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { LoadingSpinner } from '../components/LoadingSpinner';
+
+async function userAuth() {
+	var response = await fetch('/request/getloginstatus', {
+		method: "POST",
+	});
+	response = await response.json()
+	return response.auth;
+}
+
+const PrivateRoutes = () => {
+	const [state, setState] = useState("loading");
+
+	// Get login status from server
+	useEffect(() => {
+		// REMOVE TIMEOUT FOR TESTING PURPOSES ONLY.
+		setTimeout(() => {
+			(async function() {
+				try {
+					const isUserLogged = await userAuth();
+					setState(isUserLogged ? true : false);
+				} catch {
+					setState(false);
+				}
+			})();
+		}, 1000)
+	}, []);
+	if(state === "loading") {
+		return <LoadingSpinner />
+	}
+
+	return (
+		state ?  <Outlet /> : <Navigate to="/login" />
+	)
+}
+
+export default PrivateRoutes
