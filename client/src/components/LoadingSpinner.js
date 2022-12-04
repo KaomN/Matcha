fae99ce5ef1f2e64118487dfdbf68208aabfc09d
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-04 22:38:30 +0200

    modeified tabs

--- client/src/components/LoadingSpinner.js
+++ client/src/components/LoadingSpinner.js
@@ -2,20 +2,20 @@ import { Oval } from 'react-loader-spinner'
 
 export const LoadingSpinner = () => {
 	return (
-				<div>
-					{
-						<Oval
-						height={80}
-						width={80}
-						color="var(--color-primary)"
-						secondaryColor="darkslategray"
-						visible={true}
-						ariaLabel='oval-loading'
-						strokeWidth={6}
-						strokeWidthSecondary={2}
-						wrapperClass="loader"
-						/>
-					}
-				</div>
+			<div>
+				{
+					<Oval
+					height={80}
+					width={80}
+					color="var(--color-primary)"
+					secondaryColor="darkslategray"
+					visible={true}
+					ariaLabel='oval-loading'
+					strokeWidth={6}
+					strokeWidthSecondary={2}
+					wrapperClass="loader"
+					/>
+				}
+			</div>
 	)
 };
--- client/src/components/LoadingSpinnerPromise.js
+++ client/src/components/LoadingSpinnerPromise.js
@@ -4,23 +4,23 @@ import { Oval } from 'react-loader-spinner'
 export const LoadingSpinnerPromise = () => {
 	const { promiseInProgress } = usePromiseTracker();
 	return (
-				<div>
-					{
-						(promiseInProgress === true) ?
-						<Oval
-						height={80}
-						width={80}
-						color="var(--color-primary)"
-						secondaryColor="darkslategray"
-						visible={true}
-						ariaLabel='oval-loading'
-						strokeWidth={6}
-						strokeWidthSecondary={2}
-						wrapperClass="loader"
-						/>
-						:
-							null
-					}
-				</div>
+			<div>
+				{
+					(promiseInProgress === true) ?
+					<Oval
+					height={80}
+					width={80}
+					color="var(--color-primary)"
+					secondaryColor="darkslategray"
+					visible={true}
+					ariaLabel='oval-loading'
+					strokeWidth={6}
+					strokeWidthSecondary={2}
+					wrapperClass="loader"
+					/>
+					:
+						null
+				}
+			</div>
 	)
 };
