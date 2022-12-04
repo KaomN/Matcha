143cdcadd6b474d69c6efb33dbc382ac6b729632
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-04 13:49:21 +0200

    Not in use currently, Commented out some code

--- client/src/components/UserContext.js
+++ client/src/components/UserContext.js
@@ -1,20 +1,23 @@
 import {useState, createContext, useEffect} from 'react';
 
+// var response;
+// (async  () => {
+// 	response = await fetch('/request/getloginstatus', {
+// 		method: "POST",
+// 	});
+// 	response = await response.json()
+
+// })()
+
+
 export const UserContext = createContext({ name: '', auth: false });
 
 export const UserProvider = ({ children }) => {
-	const [user, setUser] = useState({ name: '', auth: false });
-	useEffect(() => {
-		(async  () => {
-			var response = await fetch('/request/getloginstatus', {
-				method: "POST",
-			});
-			response = await response.json()
-			if (response.status) {
-				setUser({name:response.name, auth:true})
-			}
-		})()
-	}, []);
+	const [user, setUser] = useState({ name: localStorage.getItem('username') || "", auth: localStorage.getItem('auth') ? true : false});
+	// useEffect(() => {
+	// 	if (localStorage.getItem('username') != null) 
+	// 		setUser({name: localStorage.getItem('username'), auth: true})
+	// }, []);
 
 	// Login updates the user data with a name parameter
 	const login = (name) => {
