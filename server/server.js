3326362b9cc379a90907230896dda9d2ea03354d
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-04 22:37:21 +0200

    added cors and cookie-parser

--- server/server.js
+++ server/server.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const session = require('express-session')
 const bodyParser = require('body-parser');
-const app = express();
 const Database = require("./createDatabase");
 const fileUpload = require('express-fileupload');
-var fs = require('fs');
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 Database.createDatabase();
+const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config({path: __dirname + '/.env'});
 const PORT = process.env.PORT;
-//sessionStore = new session.MemoryStore();
-var MySQLStore = require('express-mysql-session')(session);
+
+const MySQLStore = require('express-mysql-session')(session);
 const connection = require("./setup").pool;
-var sessionStore = new MySQLStore({}, connection);
+const sessionStore = new MySQLStore({}, connection);
+const app = express();
 app.use(session({
+	name: "sessionId",
 	secret: process.env.SESSION_SECRET,
 	store: sessionStore,
 	resave: false,
-	saveUninitialized: true
+	saveUninitialized: true,
+	cookie: {
+		maxAge: 86400000 // Session cookie persists for a day.
+	}
 }));
 
 if (!fs.existsSync(__dirname + "/uploads")){
@@ -29,18 +35,13 @@ if (!fs.existsSync(__dirname + "/uploads")){
 app.use(bodyParser.json());
 // for file uploads
 app.use(fileUpload());
-
-app.get('/verification', (req, res) => {
-	console.log("test")
-});
+app.use(cors({origin: "http://127.0.0.1:3001", credentials:true}));
+app.use(cookieParser());
 
 // UserModule
 const userModule = require('./modules/UserModule');
 app.use('/request', userModule);
 
-app.post('/upload', function(req, res) {
-	console.log(req.files)
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
