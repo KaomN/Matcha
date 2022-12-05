1918411bac9b1e07254c2990ea15ff419bbe0ec9
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-05 17:39:00 +0200

    testing home page fetching

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -5,9 +5,18 @@ import "./styles/Home.css";
 export default function Home() {
 	//const { user, logout} = useContext(UserContext);
 
+	async function handleTest() {
+		var response = await fetch('/request/test')
+		response = await response.json();
+		console.log(response)
+	}
+
 	return (
-		<main>
+		<main className="flex-col flex-center">
 			<h3>Show profile of other interesting users. Be able to filter by age gap, fame rating gap, location and interest. Also able to sort by them.</h3>
+			<div>
+				<button className="form_button" onClick={handleTest}>test</button>
+			</div>
 		</main>
 	);
 }
