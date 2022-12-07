0bbad5a6095b943a0b75018cd88e60fa67b03fc3
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-07 15:39:56 +0200

    Moved header to its own container

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,12 @@
 import React from "react";
 import "./App.css";
 import Routes from "./Routes";
-import { useLocation, useNavigate } from "react-router-dom";
-import { trackPromise} from 'react-promise-tracker';
-
-function Header() {
-	const { pathname } = useLocation();
-	const navigate = useNavigate();
-
-	function fetchLogout() {
-		const promise = new Promise((resolve, reject) => {
-			setTimeout(() => {
-				resolve(fetch('/request/logout')
-				.then((response) => response.json()));
-			}, 500)
-		});
-		return promise
-	}
-
-	async function handleLogout(event) {
-		event.preventDefault();
-			var response = await trackPromise(fetchLogout());
-			if(response.status) {
-				navigate("/login");
-			}
-			//console.log(response)
-	}
-
-	if(pathname === "/completeprofile") {
-		return (<header>
-			<div className="flex-center">
-				<a href="/" draggable="false" className="title"><h3>Matcha</h3></a>
-			</div>
-			<nav>
-				<ul>
-					<li><a href="/login" draggable="false" className="signup" title="Logout">Logout</a></li>
-				</ul>
-			</nav>
-		</header>);
-	} else if(pathname === "/home") {
-	return (<header>
-				<div className="flex-center">
-					<a href="/" draggable="false" className="title"><h3>Matcha</h3></a>
-				</div>
-				<nav>
-					<ul>
-						<li><a href="#" draggable="false" className="login" title="Logout" onClick={handleLogout}>Logout</a></li>
-					</ul>
-				</nav>
-			</header>);
-	} else {
-		return (<header>
-			<div className="flex-center">
-				<a href="/" draggable="false" className="title"><h3>Matcha</h3></a>
-			</div>
-			<nav>
-				<ul>
-					<li><a href="/login" draggable="false" className="login" title="Login">Login</a></li>
-					<li><a href="/signup" draggable="false" className="signup" title="Signup">Signup</a></li>
-				</ul>
-			</nav>
-		</header>);
-	}
-};
+import Header from './containers/Header'
 
 function Footer() {
 	return <footer>© 2022 Matcha. conguyen</footer>;
 };
 function App() {
-	// const [data, setData] = React.useState(null);
-
-	// React.useEffect(() => {
-	// 	fetch("/createDatabase")
-	// 	.then((res) => res.json())
-	// 	.then((data) => setData(data.message));
-	// }, []);
-	// console.log(data)
 	return (
 		<div className="App">
 			<Header />
