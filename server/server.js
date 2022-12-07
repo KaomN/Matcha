d8f8e2050d9dccaf73246f46873c6a0771e090e1
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-07 15:38:41 +0200

    Testing socket.io for chat, notifications and more

--- server/server.js
+++ server/server.js
@@ -10,26 +10,40 @@ const dotenv = require('dotenv');
 dotenv.config({path: __dirname + '/.env'});
 const PORT = process.env.PORT;
 const MySQLStore = require('express-mysql-session')(session);
-const connection = require("./setup").pool;
-const sessionStore = new MySQLStore({}, connection);
 const app = express();
+const server = require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.use(session({
 	name: "sessionId",
 	secret: process.env.SESSION_SECRET,
-	store: sessionStore,
+	store: new MySQLStore({}, require("./setup").pool),
 	resave: false,
 	saveUninitialized: true,
 	clearExpired: true,
 	checkExpirationInterval: 900000, // Clears expired sessions every 15minutes
-	expiration: 86400000, // Set session expiration 1day
+	expiration: 604800000 , // Set session expiration 1 week 
 	cookie: {
-		maxAge: 86400000 // Session cookie persists for a day.
+		maxAge: 604800000  // Session cookie persists for 1 week.
 	}
 }));
 
 if (!fs.existsSync(__dirname + "/uploads")){
 	fs.mkdirSync(__dirname + "/uploads");
 }
+
+// io.use( async (socket) => {
+// 	console.log("socket") 
+// });
+
+// io.on('connection', (socket) => {
+// 	console.log(`a user connected ID: ${socket.id}: ${"s"}`);
+// });
+
+// io.on('disconnect', (socket) => {
+// 	console.log('a user disconnected');
+// });
 
 // For parsing application/json header
 app.use(bodyParser.json());
@@ -38,10 +52,10 @@ app.use(fileUpload());
 app.use(cors({origin: "http://127.0.0.1:3001", credentials:true}));
 
 // UserModule
-const userModule = require('./modules/UserModule');
-app.use('/request', userModule);
+//const userModule = require('./controllers/UserController');
+app.use('/request', require('./controllers/UserController'));
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
 });
