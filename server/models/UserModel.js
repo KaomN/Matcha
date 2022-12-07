c5f6d1f800f2281216c11c3c0fd4f9ab40a7c742
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-07 15:37:42 +0200

    Refacteroed code, split up Module to Controller and Model

---
+++ server/models/UserModel.js
@@ -0,0 +1,369 @@
+const bcrypt = require('bcrypt');
+const crypto = require('crypto')
+const con = require("../setup").pool;
+const emailTransporter =  require("../setup").emailTransporter;
+var fs = require('fs');
+// const axios = require('axios');
+// const dotenv = require('dotenv');
+// dotenv.config({path: __dirname + '/.env'});
+
+// Register request
+const register = async (req) => {
+	const { firstname, surname, username, email, password} = req.body;
+	try {
+		var [rows, fields] = await con.execute('SELECT * FROM users WHERE username = ?', [username])
+		console.log(rows[0])
+		if(rows[0] === undefined) {
+			var [rows, fields] = await con.execute("SELECT * FROM users WHERE email = ?", [email])
+			if (rows[0] === undefined) {
+				const hash = await bcrypt.hash(password, 10);
+				let token = crypto.createHash('md5').update(username).digest("hex") + crypto.createHash('md5').update(email).digest("hex")
+				result = await con.execute(`INSERT INTO users (firstname, surname, username, email, password, token)
+											VALUE (?, ?, ?, ?, ?, ?);`,
+											[firstname, surname, username, email, hash, token])
+				if (result) {
+					const mailOptions = {
+						from: 'kaom.n.92@gmail.com',
+						to: email,
+						subject: 'Matcha account confirmation',
+						text: 'Please follow the link below to verify your account.\n' + 'http://localhost:3000?verification=' + token
+					};
+					result = await emailTransporter.sendMail(mailOptions);
+					if (result.messageId) {
+						return ({status:true});
+					} else {
+						return ({status: false, message: "Server connection error"});
+					}
+				} else {
+					return ({status: false, message: "Server connection error"});
+				}
+			} else {
+				return ({errorEmail: "Email taken!", "status": false});
+			}
+		} else {
+			console.log("this")
+			return {errorUsername: "Username taken!", "status": false};
+		}
+	} catch {
+		return ({status: false, message: "Server connection error"});
+	}
+}
+
+const login = async (req) => {
+	const { firstname, surname, username, email, password} = req.body;
+	try {
+		var [rows, fields] = await con.execute(`SELECT *
+												FROM users
+												WHERE username = ?`,
+												[username])
+		if (rows[0] === undefined) {
+			return ({status: false, error: "Incorrect username/password"});
+		} else {
+			if(rows[0].verified === 0) {
+				return ({"status": false, "verified": false});
+			} else {
+				const match = await bcrypt.compare(password, rows[0].password)
+				if(match) {
+					req.session.username = rows[0].username;
+					req.session.userid = rows[0].pk_userid;
+					req.session.firstname = rows[0].firstname;
+					req.session.surname = rows[0].surname;
+					req.session.email = rows[0].email;
+					req.session.rating = rows[0].rating;
+					req.session.token = rows[0].token;
+					req.session.verified = rows[0].verified;
+					req.session.gender = rows[0].gender
+					req.session.preference = rows[0].genderpreference
+					req.session.latitude = rows[0].latitude
+					req.session.longitude = rows[0].longitude
+					// Creating User folder
+					if (!fs.existsSync(__dirname.slice(0, -7) + "/uploads/" + req.session.username)){
+						fs.mkdirSync(__dirname.slice(0, -7) + "/uploads/" + req.session.username);
+					}
+					if(rows[0].profile === 1)
+						return ({status: true, profile: true});
+					else
+					return ({status: true, profile: false});
+				} else {
+					return ({status: false, error: "Incorrect username/password"});
+				}
+			}
+		}
+	} catch (err) {
+		console.log(err)
+		return ({status: false, message: "Server connection error"});
+	}
+}
+
+// Verify account request
+const verify = async (req) => {
+	try {
+		var [rows, fields] = await con.execute(`SELECT *
+												FROM users
+												WHERE token = ?`,
+												[req.body.token])
+		console.log(rows)
+		if(rows[0] === undefined) {
+			return ({"status": false});
+		} else if (rows[0].verified === 0) {
+			var result = await con.execute("UPDATE users SET users.VERIFIED = 1 WHERE token = ?;", [req.body.token])
+				if (result) {
+					return ({"status": true, "verified": false});
+				} else {
+					return ({status: false, message: "Server connection error"});
+				}
+		} else {
+			return ({"status": true, "verified": true});
+		}
+	} catch(err) {
+		console.log(err)
+		return ({status: false, message: "Server connection error"});
+	}
+};
+
+// Forgot password request
+const forgotPassword = async (req, res) => {
+	let email = req.body.email;
+	try {
+		var [rows, fields] = await con.execute(`SELECT *
+												FROM users
+												WHERE email = ?`,
+												[email])
+		if (rows[0] !== undefined) {
+			const token = crypto.createHash('md5').update(rows[0].token).digest("hex") + crypto.createHash('md5').update(String(Date.now())).digest("hex")
+			var result = await con.execute(`INSERT INTO usertokens (pk_userid, passwordresettoken, passwordresetexpr)
+											VALUES (?, ?, ?)
+											ON DUPLICATE KEY UPDATE passwordresettoken = VALUES(passwordresettoken), passwordresetexpr = VALUES(passwordresetexpr)`,
+											[rows[0].pk_userid, token, Math.floor(Date.now() / 1000)])
+			if (result) {
+				const mailOptions = {
+					from: 'kaom.n.92@gmail.com',
+					to: email,
+					subject: 'Matcha password reset request',
+					text: 'Please follow the link below to reset account password.\n' + 'http://localhost:3000/passwordreset?token=' + token
+				};
+				result = await emailTransporter.sendMail(mailOptions)
+					if (result.messageId) {
+						return ({"status": true, "message": "An email has been sent to " + email + ". Please follow instructions on the email to reset your password!"});
+					} else {
+						return ({status: false, message: "Server connection error"});
+					}
+			} else {
+				return ({status: false, message: "Server connection error"});
+			}
+		} else {
+			return ({"status": true, "message": "An email has been sent to " + email + ". Please follow instructions on the email to reset your password!"});
+		}
+	} catch {
+		return ({status: false, message: "Server connection error"});
+	}
+};
+
+// Password reset request
+const passwordReset = async (req) => {
+	const {password, passwordConfirm, token} = req.body;
+	try {
+		var [rows, fields] = await con.execute(`SELECT *
+												FROM usertokens
+												WHERE passwordresettoken = ?`,
+												[token])
+		if (rows[0] !== undefined) {
+			if(rows[0].passwordresetexpr + 86400 > Date.now() / 1000) {
+				const hash = await bcrypt.hash(password, 10);
+				var result = await con.execute(`UPDATE users
+												SET password = ?
+												WHERE pk_userid = ?`,
+												[hash, rows[0].pk_userid])
+				if (result) {
+					result = await con.execute(`DELETE FROM usertokens
+												WHERE pk_userid = ?`,
+												[rows[0].pk_userid])
+					if (result) {
+						return ({"status": true, "message": "Password reset!"})
+					} else {
+						return ({status: false, message: "Server connection error"});
+					}
+				} else {
+					return ({status: false, message: "Server connection error1"});
+				}
+			} else {
+				return ({"status": false, "error": "Password reset link expired!"})
+			}
+		} else {
+			return ({"status": false, "error": "Please follow the link you received on your email to reset your account password"})
+		}
+	} catch (err) {
+		return ({status: false, message: "Server connection error2"});
+	}
+};
+
+// Complete profile on First login.
+const completeProfile = async (req, res) => {
+	//console.log(__dirname.slice(0, -7) + "uploads")
+	const {age, birthDate, gender, preference, biography, locationLat, locationLng, interest} = req.body;
+	var error = {};
+	function uploadProfilePicture(profilePic, path) {
+		path += "profile.jpg"
+		profilePic.mv(path, function(err) {
+			if (err)
+				return false;
+		});
+		return true;
+	}
+
+	function uploadPictures(picture, path, picturename) {
+		path += picturename + ".jpg"
+		picture.mv(path, function(err) {
+			if (err)
+				return false;
+		});
+		return true;
+	}
+	try {
+		// Pictures
+		var uploadPath = __dirname.slice(0, -6) + "uploads/"  + req.session.username + "/" // Change to username later
+		for (let imageName in req.files) {
+			if (imageName === "profilePicture") {
+				// Uploads Profile picture to server.
+				if(uploadProfilePicture(req.files[imageName], uploadPath)) {
+					var profileId = 1;
+					var profileName = "profile.jpg"
+					var result = await con.execute(`INSERT INTO images(fk_userid, profilepic, imagename)
+													VALUES (?, ?, ?)`,
+													[req.session.userid, profileId, profileName])
+					//console.log(result)
+					if (!result) {
+						Object.assign(error, {error: "Something went wrong!"});
+					}
+				}
+			} else {
+				if(uploadPictures(req.files[imageName], uploadPath, imageName)) {
+						var profileName = imageName + ".jpg"
+						var result = await con.execute(`INSERT INTO images (fk_userid, imagename)
+														VALUES (?, ?)`,
+														[req.session.userid, profileName])
+						if (!result) {
+							Object.assign(error, {error: "Something went wrong!"});
+						}
+				}
+			}
+		}
+		// Interest
+		if(error && Object.keys(error).length === 0 && Object.getPrototypeOf(error) === Object.prototype) {
+			const interestArr = interest.split(' ')
+			for (const interest of interestArr) {
+				var [rows, fields] = await con.execute(`SELECT *
+														FROM tag
+														WHERE tag = ?`,
+														[interest])
+				if (!rows[0]) {
+					var result = await con.execute(`INSERT INTO tag (tag)
+													VALUES (?)`,
+													[interest])
+					var result = await con.execute(`INSERT INTO tagitem (fk_userid, fk_tagid)
+													VALUES (?, ?)
+													ON DUPLICATE KEY UPDATE tagitem.fk_tagid = tagitem.fk_tagid`,
+													[req.session.userid, result[0].insertId])
+					if (!result) {
+						Object.assign(error, {error: "Something went wrong!"});
+					}
+				} else {
+					var [rows, fields] = await con.execute(`SELECT *
+															FROM tag
+															WHERE tag = ?`,
+															[interest])
+					var result = await con.execute(`INSERT INTO tagitem (fk_userid, fk_tagid)
+													VALUES (?, ?)
+													ON DUPLICATE KEY UPDATE tagitem.fk_tagid = tagitem.fk_tagid`,
+													[req.session.userid, rows[0].pk_tagid])
+					if (!result) {
+						Object.assign(error, {error: "Something went wrong!"});
+					}
+				}
+			}
+		} else {
+			return (error);
+		}
+		// User info
+		if(error && Object.keys(error).length === 0 && Object.getPrototypeOf(error) === Object.prototype) {
+			var result = await con.execute(`UPDATE users
+											SET users.gender = ?, users.age = ?, users.dateofbirth = ?, users.genderpreference = ?, users.biography = ?, users.latitude = ?, users.longitude = ?, users.profile = 1
+											WHERE users.pk_userid = ?`,
+											[gender, age, birthDate, preference, biography, locationLat, locationLng, req.session.userid])
+			if (!result) {
+				Object.assign(error, {error: "Something went wrong!"});
+			} else {
+				req.session.gender = gender
+				req.session.preference = preference
+				req.session.latitude = locationLat
+				req.session.longitude = locationLng
+				req.session.age = age
+				req.session.interest = interest
+				return ({status:true})
+			}
+		} else {
+			return (error);
+		}
+	} catch (err) {
+		return ({status: false, message: "Server connection error"});
+	}
+};
+
+const test = async (req) => {
+	//console.log(req.session.preference)
+
+	function distance(lat1, lng1, lat2, lng2) {
+		lng1 =  lng1 * Math.PI / 180;
+		lng2 = lng2 * Math.PI / 180;
+		lat1 = lat1 * Math.PI / 180;
+		lat2 = lat2 * Math.PI / 180;
+
+		let dlon = lng2 - lng1;
+		let dlat = lat2 - lat1;
+		 // Haversine formula
+		let a = Math.pow(Math.sin(dlat / 2), 2)
+				+ Math.cos(lat1) * Math.cos(lat2)
+				* Math.pow(Math.sin(dlon / 2),2);
+			
+		let c = 2 * Math.asin(Math.sqrt(a));
+
+		// Radius of earth in kilometers. Use 3956 for miles
+		let r = 6371;
+
+		// calculate the result
+		return(parseInt(c * r));
+	}
+
+	try {
+		if (req.session.preference === "both") {
+			
+		} else {
+			// Get all users that match with users gender preference
+			var [rows, fields] = await con.execute(`SELECT username, age, firstname, surname, latitude, longitude
+													FROM users
+													WHERE NOT pk_userid = ? AND gender = ? AND genderpreference = ? OR genderpreference = "both" AND NOT gender = ?`,
+													[req.session.userid, req.session.preference, req.session.gender, req.session.gender])
+			//var [rows, fields] = await con.execute('SELECT * FROM users WHERE NOT pk_userid = ? AND gender = ?', [req.session.userid, req.session.preference])
+			// calculate distance between user1 and user2
+			for (const user of rows) {
+				user.distance = distance(req.session.latitude, req.session.longitude, user.latitude, user.longitude) + "km away"
+				console.log(distance(req.session.latitude, req.session.longitude, user.latitude, user.longitude))
+			}
+			console.log(rows)
+		}
+		return (rows)
+	} catch (err) {
+		console.log(err)
+		return({status: false, message: "Server connection error"});
+	}
+};
+
+module.exports = {
+	register,
+	login,
+	verify,
+	forgotPassword,
+	passwordReset,
+	completeProfile,
+	test,
+}
