13fccbf5cbfa5b08edf71d45f2d99d85e2a2b51d
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-12 16:49:22 +0200

    added socketContext

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import { UserProvider } from './components/UserContext';
+//import { UserProvider } from './components/UserContext';
 import { LoadingSpinnerPromise } from './components/LoadingSpinnerPromise';
+import {SocketContext, socket} from './context/socket';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Router>
-		<UserProvider>
+		{/* <UserProvider> */}
+		<SocketContext.Provider value={socket}>
 			<App />
 			<LoadingSpinnerPromise />
-		</UserProvider>
+		</SocketContext.Provider>
+		{/* </UserProvider> */}
 	</Router>
 );
