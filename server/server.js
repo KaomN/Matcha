9875a0d5ab47e64306a37f859a4c950a625e27aa
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-12 16:47:19 +0200

    added sessionMiddleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ dotenv.config({path: __dirname + '/.env'});
 const PORT = process.env.PORT;
 const MySQLStore = require('express-mysql-session')(session);
 const {app, server} = require('./modules/SocketIO');
+const {sessionMiddleware} = require('./modules/SessionMiddleware')
 
 //const app = express();
 // const server = require('http').createServer(app);
@@ -20,19 +21,23 @@ const {app, server} = require('./modules/SocketIO');
 // 		methods: ['GET', 'POST'],
 // 	},
 // });
-app.use(session({
-	name: "sessionId",
-	secret: process.env.SESSION_SECRET,
-	store: new MySQLStore({}, require("./setup").pool),
-	resave: false,
-	saveUninitialized: true,
-	clearExpired: true,
-	checkExpirationInterval: 900000, // Clears expired sessions every 15minutes
-	expiration: 604800000 , // Set session expiration 1 week
-	cookie: {
-		maxAge: 604800000  // Session cookie persists for 1 week.
-	}
-}));
+
+// app.use(session({
+// 	name: "sessionId",
+// 	secret: process.env.SESSION_SECRET,
+// 	store: new MySQLStore({}, require("./setup").pool),
+// 	resave: false,
+// 	saveUninitialized: true,
+// 	clearExpired: true,
+// 	checkExpirationInterval: 900000, // Clears expired sessions every 15minutes
+// 	expiration: 604800000 , // Set session expiration 1 week
+// 	cookie: {
+// 		maxAge: 604800000  // Session cookie persists for 1 week.
+// 	}
+// }));
+
+// Session middleware
+app.use(sessionMiddleware);
 
 if (!fs.existsSync(__dirname + "/uploads")){
 	fs.mkdirSync(__dirname + "/uploads");
@@ -52,10 +57,12 @@ app.use(fileUpload());
 app.use(cors({origin: "http://127.0.0.1:3001", credentials:true}));
 
 //const userModule = require('./controllers/UserController');
-// UserModule
+// UserController
 app.use('/request', require('./controllers/UserController'));
-
+// ChatController
 app.use('/chat', require('./controllers/ChatController'));
+// ProfileController
+app.use('/profile', require('./controllers/ProfileController'));
 
 server.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
