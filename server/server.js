21b1564555d431c2563e30e2913971bb30de8eb9
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-13 10:33:48 +0200

    remvoed extra comments

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const session = require('express-session')
+const express = require('express');
 const bodyParser = require('body-parser');
 const Database = require("./createDatabase");
 const fileUpload = require('express-fileupload');
@@ -12,43 +13,14 @@ const MySQLStore = require('express-mysql-session')(session);
 const {app, server} = require('./modules/SocketIO');
 const {sessionMiddleware} = require('./modules/SessionMiddleware')
 
-//const app = express();
-// const server = require('http').createServer(app);
-// const { Server } = require("socket.io");
-// const io = new Server(server, {
-// 	cors: {
-// 		origin: 'http://localhost:3000',
-// 		methods: ['GET', 'POST'],
-// 	},
-// });
-
-// app.use(session({
-// 	name: "sessionId",
-// 	secret: process.env.SESSION_SECRET,
-// 	store: new MySQLStore({}, require("./setup").pool),
-// 	resave: false,
-// 	saveUninitialized: true,
-// 	clearExpired: true,
-// 	checkExpirationInterval: 900000, // Clears expired sessions every 15minutes
-// 	expiration: 604800000 , // Set session expiration 1 week
-// 	cookie: {
-// 		maxAge: 604800000  // Session cookie persists for 1 week.
-// 	}
-// }));
-
 // Session middleware
 app.use(sessionMiddleware);
+//app.use(express.static('public'));
+app.use('/images', express.static(__dirname + '/uploads'));
 
 if (!fs.existsSync(__dirname + "/uploads")){
 	fs.mkdirSync(__dirname + "/uploads");
 }
-
-// Save Socket users in array;
-
-
-// io.on('disconnect', (socket) => {
-// 	console.log('a user disconnected');
-// });
 
 // For parsing application/json header
 app.use(bodyParser.json());
