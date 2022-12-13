c9635ba4f3ba5f4d6efb321d7cc98021a9baf9bd
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-13 10:35:44 +0200

    adding Profile controller

--- server/controllers/ProfileController.js
+++ server/controllers/ProfileController.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const ProfileModel = require('../models/ProfileModel');
 
 router.post("/getprofile", async (req, res) => {
+	console.log(req.body)
 	if(req.body && Object.keys(req.body).length === 0 && Object.getPrototypeOf(req.body) === Object.prototype) {
 		profileID = req.session.userid
 	} else {
 		profileID = req.body.profileID
 	}
-	res.send(await ProfileModel.getProfile(profileID))
+	//console.log(await ProfileModel.getProfile(profileID))
+	res.send(await ProfileModel.getProfile(profileID, req))
 });
 
 module.exports = router;
