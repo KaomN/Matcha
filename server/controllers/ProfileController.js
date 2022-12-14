2bb5e36f27b2147144fdb5ee0570547c17ee760a
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-14 15:52:48 +0200

    Testing Profile fetching

--- server/controllers/ProfileController.js
+++ server/controllers/ProfileController.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const ProfileModel = require('../models/ProfileModel');
 
 router.post("/getprofile", async (req, res) => {
-	console.log(req.body)
+	//console.log(req.body)
 	if(req.body && Object.keys(req.body).length === 0 && Object.getPrototypeOf(req.body) === Object.prototype) {
 		profileID = req.session.userid
 	} else {
