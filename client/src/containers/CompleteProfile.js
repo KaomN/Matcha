9976625242fb7538b33b1318e62ff71d530a9105
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-14 15:56:24 +0200

    Removed Uploading other images from complete profile, added working react-easy-crop npm module

--- client/src/containers/CompleteProfile.js
+++ client/src/containers/CompleteProfile.js
@@ -325,10 +325,11 @@ export default function FirstTimeProfile() {
 												crop={crop}
 												zoom={zoom}
 												// cropShape={"round"}
-												aspect={4 / 3}
+												aspect={4 / 4}
 												onCropChange={setCrop}
 												onCropComplete={onCropComplete}
 												onZoomChange={setZoom}
+												cropShape = 'round'
 												/>
 												:
 												null}
@@ -345,9 +346,9 @@ export default function FirstTimeProfile() {
 										if(document.getElementById('profilePic') != null) {
 											document.querySelector('.form_message_error').innerHTML = "Please choose a profile picture!"
 										} else {
-											setShowForm("pictureForm");
+											handleSubmit();
 										}
-										}}>Next</button>
+										}}>Submit</button>
 								</div>
 							</div>
 						</div>
@@ -355,69 +356,68 @@ export default function FirstTimeProfile() {
 				</main>);
 	}
 
-	function saveChosenPicture(event) {
-		if(picture.length < 4) {
-			let pictureObj = event.target.files[0]
-			if (pictureObj !== undefined) {
-				let pictureSrc = URL.createObjectURL(pictureObj)
-				setPicture([...picture, {id:++numImages, name:pictureObj, src: pictureSrc}]);
-			}
-		}
-	}
+	// function saveChosenPicture(event) {
+	// 	if(picture.length < 4) {
+	// 		let pictureObj = event.target.files[0]
+	// 		if (pictureObj !== undefined) {
+	// 			let pictureSrc = URL.createObjectURL(pictureObj)
+	// 			setPicture([...picture, {id:++numImages, name:pictureObj, src: pictureSrc}]);
+	// 		}
+	// 	}
+	// }
 
-	function PictureForm() {
-		var input;
-		if (picture.length < 4) {
-			input = <input type="file" accept="image/*" id="pictureUploads" onChange={saveChosenPicture}/>
-		} else {
-			input = "";
-		}
-		return (<main className="form-container ma">
-					<div className="complete-profile-form">
-						<div style={{backgroundColor: ""}}>
-							<h1 className="title">Complete your profile</h1>
-							<div className="form_message form_message_error"></div>
-						</div>
-							<div className="complete-form-container">
-								<div id="pictureForm">
-									<div className="flex flex-col flex-align-center">
-										<label style={{fontSize: "23px"}}>Pictures</label>
-										<div className="flex flex-col flex-align-center">
-											<p className="m-0">Choose pictures to upoad to your profile!</p>
-											<p className="mt-0">Max 4 pictures!</p>
-										</div>
-									</div>
-									<div className="flex-column-completeprofile">
-										<div style={{border: "0px", marginBottom: "0.5rem"}}>
-											{input}
-										</div>
-									</div>
-									<div className="flex-center pl-1rem pr-1rem pb-1rem complete-form-image-container flex-col flex-justify-content-start">
-										{picture.map(pictureElem => (
-										<div key={pictureElem.id} data-key={pictureElem.id} className="flex-center wh-100p flex-col flex-justify-content-start">
-											<img className="complete-form-img pt-1rem" src={pictureElem.src} alt=""/>
-											<button className="complete-form-button" onClick={() => {
-												setPicture(
-													picture.filter(a =>
-													a.id !== parseInt(pictureElem.id)
-													)
-												);
-											}}>Delete</button>
-										</div>
-										))}
-									</div>
-									<div className="center-gap">
-										<button className="complete-form-button" onClick={() => {setShowForm("profileForm");}}>Previous</button>
-										<button className="complete-form-button" onClick={handleSubmit}>Submit</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</main>);
-	}
+	// function PictureForm() {
+	// 	var input;
+	// 	if (picture.length < 4) {
+	// 		input = <input type="file" accept="image/*" id="pictureUploads" onChange={saveChosenPicture}/>
+	// 	} else {
+	// 		input = "";
+	// 	}
+	// 	return (<main className="form-container ma">
+	// 				<div className="complete-profile-form">
+	// 					<div style={{backgroundColor: ""}}>
+	// 						<h1 className="title">Complete your profile</h1>
+	// 						<div className="form_message form_message_error"></div>
+	// 					</div>
+	// 						<div className="complete-form-container">
+	// 							<div id="pictureForm">
+	// 								<div className="flex flex-col flex-align-center">
+	// 									<label style={{fontSize: "23px"}}>Pictures</label>
+	// 									<div className="flex flex-col flex-align-center">
+	// 										<p className="m-0">Choose pictures to upoad to your profile!</p>
+	// 										<p className="mt-0">Max 4 pictures!</p>
+	// 									</div>
+	// 								</div>
+	// 								<div className="flex-column-completeprofile">
+	// 									<div style={{border: "0px", marginBottom: "0.5rem"}}>
+	// 										{input}
+	// 									</div>
+	// 								</div>
+	// 								<div className="flex-center pl-1rem pr-1rem pb-1rem complete-form-image-container flex-col flex-justify-content-start">
+	// 									{picture.map(pictureElem => (
+	// 										<div key={pictureElem.id} data-key={pictureElem.id} className="flex-center wh-100p flex-col flex-justify-content-start">
+	// 											<img className="complete-form-img pt-1rem" src={pictureElem.src} alt=""/>
+	// 											<button className="complete-form-button" onClick={() => {
+	// 												setPicture(
+	// 													picture.filter(a =>
+	// 													a.id !== parseInt(pictureElem.id)
+	// 													)
+	// 												);
+	// 											}}>Delete</button>
+	// 										</div>
+	// 									))}
+	// 								</div>
+	// 								<div className="center-gap">
+	// 									<button className="complete-form-button" onClick={() => {setShowForm("profileForm");}}>Previous</button>
+	// 									<button className="complete-form-button" onClick={handleSubmit}>Submit</button>
+	// 								</div>
+	// 							</div>
+	// 						</div>
+	// 					</div>
+	// 				</main>);
+	// }
 
-	async function handleSubmit(event) {
-		event.preventDefault();
+	async function handleSubmit() {
 		const formdata = new FormData();
 		formdata.append("age", age);
 		formdata.append("birthDate", birthDate);
@@ -435,11 +435,11 @@ export default function FirstTimeProfile() {
 		formdata.append("profilePicture", profilePicture);
 		formdata.append("x", imageSize.x)
 		formdata.append("y", imageSize.y)
-		formdata.append("width", imageSize.width)
-		formdata.append("height", imageSize.height)
-		picture.map(pictureElem => (
-			formdata.append("pictureUpload" + pictureElem.id, pictureElem.name)
-		));
+		formdata.append("cropWidth", imageSize.width)
+		formdata.append("cropHeight", imageSize.height)
+		// picture.map(pictureElem => (
+		// 	formdata.append("pictureUpload" + pictureElem.id, pictureElem.name)
+		// ));
 		let response = await fetch('/request/completeprofile', {
 			method: "POST",
 			body: formdata
@@ -462,6 +462,4 @@ export default function FirstTimeProfile() {
 		return InterestForm()
 	else if(showform === "profileForm")
 		return ProfileForm()
-	else if(showform === "pictureForm")
-		return PictureForm()
 }
