75c4407638950fe63ca1f5a24c5557f037daa608
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-14 15:52:36 +0200

    Testing Profile fetching

--- server/models/ProfileModel.js
+++ server/models/ProfileModel.js
@@ -5,12 +5,30 @@ const calculate = require('../modules/CalculateDistance');
 const getProfile = async (userID, req) => {
 	//const { firstname, surname, username, email, password } = req.body;
 	try {
+		var imageCount = 0
+		// Fetch user info
 		var [rows, fields] = await con.execute(`SELECT username, firstname, surname, gender, age, dateofbirth, biography, latitude, longitude, pk_userid as 'userid', rating
 												FROM users
 												WHERE pk_userid = ?`,
 												[userID])
-		console.log(rows[0])
 		if(rows[0] !== undefined) {
+			// Fetch images
+			var result = await con.execute(`SELECT *
+											FROM images
+											WHERE fk_userid = ?`,
+											[userID])
+											console.log(rows[0])
+			if (result[0] !== undefined) {
+				Object.assign(rows[0], {profile: false})
+				for (const images of result[0]) {
+					if (images.profilepic === 1) {
+						Object.assign(rows[0], {profile: true})
+						Object.assign(rows[0], {profileSrc: "http://localhost:3001/images/" + req.session.username + "/profile.jpg"})
+					} else {
+						imageCount++;
+					}
+				}
+			}
 			if(rows[0].userid === req.session.userid) {
 				Object.assign(rows[0], {isOwn: true})
 				Object.assign(rows[0], {status: true})
@@ -18,8 +36,9 @@ const getProfile = async (userID, req) => {
 			}
 			Object.assign(rows[0], {distance: calculate.distance(req.session.latitude, req.session.longitude, rows[0].latitude, rows[0].longitude)})
 			Object.assign(rows[0], {isOwn:false})
-			console.log(rows[0])
+			//console.log(rows[0])
 			Object.assign(rows[0], {status: true})
+			
 			return (rows[0]);
 		} else {
 			return ({status: false});
