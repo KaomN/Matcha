75671c970486898f7b03b0f1db65e74b22e6e685
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-14 15:57:16 +0200

    Testing userContext

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,16 +1,15 @@
-//import { useContext } from "react";
-//import { UserContext } from '../components/UserContext';
+// import { useContext } from "react";
+// import { UserContext } from '../context/UserContext';
 import "./styles/Home.css";
 
 export default function Home() {
-	//const { user, logout} = useContext(UserContext);
+	//const { user } = useContext(UserContext);
 
 	async function handleTest() {
 		var response = await fetch('/request/test')
 		response = await response.json();
 		console.log(response)
 	}
-
 	return (
 		<main className="flex-col flex-center ma">
 			<h3>Show profile of other interesting users. Be able to filter by age gap, fame rating gap, location and interest. Also able to sort by them.</h3>
