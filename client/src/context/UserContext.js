135d51bf7b2ebe9c5095cfc91f92d4ccbe95c7fd
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-16 10:37:54 +0200

    Modified Usercontext

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,18 +1,19 @@
 import {useState, createContext, useEffect} from 'react';
 
-export const UserContext = createContext({ name: "", auth: false });
+export const UserContext = createContext({ name: "", auth: false, profilePic: false});
 
 export const UserProvider = ({ children }) => {
-	const [user, setUser] = useState({ name: "", auth: false});
+	const [user, setUser] = useState({ name: "", auth: false, profilePic: false});
 	useEffect(() => {
 		(async () => {
 			var response = await fetch('/request/getloginstatus');
 			response = await response.json()
-			setUser({name: response.username, auth: response.auth})
+			setUser({name: response.username, auth: response.auth, profilePic: response.profilePic})
 		})()
 	}, []);
+	//console.log(user)
 	return (
-		<UserContext.Provider value={{ user }}>
+		<UserContext.Provider value={{ user, setUser }}>
 			{children}
 		</UserContext.Provider>
 	);
