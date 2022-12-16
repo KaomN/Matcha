414ecd6c002a43e7be7aa2e68488ff000be596cc
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-16 10:38:42 +0200

    Added dropdown menus

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,14 +1,89 @@
-//import { useContext } from "react";
-//import { UserContext } from '../components/UserContext';
 import { useLocation, useNavigate } from "react-router-dom";
 import { trackPromise} from 'react-promise-tracker';
-import { useContext } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { UserContext } from '../context/UserContext';
+import "./styles/Header.css";
+
+
+function useProfileVisible(profileInitialIsVisible) {
+	const [isProfileVisible, setIsProfileVisible] = useState(
+		profileInitialIsVisible
+	);
+	const refProfile = useRef(null);
+
+	const handleClickOutside = event => {
+		if (refProfile.current && !refProfile.current.contains(event.target)) {
+			setIsProfileVisible(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", handleClickOutside, true);
+		return () => {
+			document.removeEventListener("click", handleClickOutside, true);
+		};
+	});
+
+	return { refProfile, isProfileVisible, setIsProfileVisible };
+}
+
+function useChatVisible(chatInitialIsVisible) {
+	const [isChatVisible, setIsChatVisible] = useState(
+		chatInitialIsVisible
+	);
+	const refChat = useRef(null);
+
+	const handleClickOutside = event => {
+		if (refChat.current && !refChat.current.contains(event.target)) {
+			setIsChatVisible(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", handleClickOutside, true);
+		return () => {
+			document.removeEventListener("click", handleClickOutside, true);
+		};
+	});
+
+	return { refChat, isChatVisible, setIsChatVisible };
+}
+
+function useNotificationVisible(notificationInitialIsVisible) {
+	const [isNotificationVisible, setIsNotificationVisible] = useState(
+		notificationInitialIsVisible
+	);
+	const refNotification = useRef(null);
+
+	const handleClickOutside = event => {
+		if (refNotification.current && !refNotification.current.contains(event.target)) {
+			setIsNotificationVisible(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", handleClickOutside, true);
+		return () => {
+			document.removeEventListener("click", handleClickOutside, true);
+		};
+	});
+
+	return { refNotification, isNotificationVisible, setIsNotificationVisible };
+}
 
 export default function Header() {
 	const { user } = useContext(UserContext);
+	const [profileImageSrc, setProfileImageSrc] = useState(false);
 	const { pathname } = useLocation();
 	const navigate = useNavigate();
+
+	const [profileDropdown, setProfileDropdown] = useState("");
+	const [chatDropdown, setChatDropdown] = useState("");
+	const [notificationDropdown, setNotificationDropdown] = useState("");
+
+	const { refProfile, isProfileVisible, setIsProfileVisible } = useProfileVisible(false);
+	const { refChat, isChatVisible, setIsChatVisible } = useChatVisible(false);
+	const { refNotification, isNotificationVisible, setIsNotificationVisible } = useNotificationVisible(false);
 
 	function fetchLogout() {
 		const promise = new Promise((resolve, reject) => {
@@ -27,18 +102,16 @@ export default function Header() {
 				navigate("/login");
 			}
 	}
-	if(pathname === "/completeprofile") {
-		return (<header>
-			<div className="flex-center">
-				<a href="/" draggable="false" className="title"><h3>Matcha</h3></a>
-			</div>
-			<nav>
-				<ul>
-					<li><a href="/login" draggable="false" className="signup" title="Logout">Logout</a></li>
-				</ul>
-			</nav>
-		</header>);
-	} else if(pathname === "/login" || pathname === "/signup" || pathname === "/forgotpassword" || pathname === "/passwordreset" || pathname === "/") {
+
+	useEffect(() => {
+		(async () => {
+			var response = await fetch('/request/getprofileimage');
+			response = await response.json()
+			setProfileImageSrc(response.imageSrc)
+		})()
+	}, [pathname]);
+	//console.log(profileImageSrc)
+	if(pathname === "/login" || pathname === "/signup" || pathname === "/forgotpassword" || pathname === "/passwordreset" || pathname === "/") {
 		return (<header>
 			<div className="flex-center">
 				<a href="/" draggable="false" className="title"><h3>Matcha</h3></a>
@@ -55,12 +128,52 @@ export default function Header() {
 			<div className="flex-center">
 				<a href="/home" draggable="false" className="title"><h3>Matcha</h3></a>
 			</div>
-			<nav>
-				<ul>
-					<li><i className="material-icons" onClick={handleLogout} title="Logout">logout</i></li>
-					<li><i className="material-icons">notifications</i></li>
-					<li><i className="material-icons">chat</i></li>
-					<li>{user.name === "" ? null : <img className="header-profile" src={"http://localhost:3001/images/" + user.name + "/profile.jpg"}></img>}</li>
+			<nav className="pr05">
+				<ul className="header-overflow-visible">
+					<div className="header-profile-dropdown-container">
+						<li><i className="material-icons" onClick={handleLogout} title="Logout">logout</i></li>
+						<li>
+							<div ref={refNotification}>
+								<i className="material-icons" onClick={ () => isNotificationVisible ? setIsNotificationVisible(false) : setIsNotificationVisible(true) }>notifications</i>
+								{!isNotificationVisible ? null : 
+									<div className="header-profile-dropdown">
+										<div className="header-profile-dropdown-profile-container">
+											Show Notification
+										</div>
+									</div>
+								}
+							</div>
+						</li>
+						<li>
+							<div ref={refChat}>
+								<i className="material-icons" onClick={ () => isChatVisible ? setIsChatVisible(false) : setIsChatVisible(true) }>chat</i>
+								{!isChatVisible ? null : 
+									<div className="header-profile-dropdown">
+										<div className="header-profile-dropdown-profile-container">
+											Show connected persons
+										</div>
+									</div>
+								}
+							</div>
+						</li>
+						<li>
+							{profileImageSrc === false ? null :
+								<div ref={refProfile}>
+									<img className="header-profile" src={profileImageSrc} onClick={ () => isProfileVisible ? setIsProfileVisible(false) : setIsProfileVisible(true) }></img>
+										{!isProfileVisible ? null : 
+											<div className="header-profile-dropdown">
+											<div className="header-profile-dropdown-profile-container">
+												<div className="header-profile-dropdown-profile">Show own profile</div>
+												<div className="header-profile-seperator"></div>
+												<div className="header-profile-dropdown-profile-history">Profile history</div>
+											</div>
+											<div className="header-profile-dropdown-profile">Settings? needed?</div>
+										</div>
+										}
+								</div>
+							}
+						</li>
+					</div>
 				</ul>
 			</nav>
 		</header>);
