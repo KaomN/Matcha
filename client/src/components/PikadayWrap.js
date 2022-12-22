777af0fc2b6b3fecca8711d2e7557666f9f6fa30
Cong Nguyen <kaom.n.92@gmail.com>
2022-12-22 09:27:27 +0200

    Added props to automatically pick current user date in profile settings

--- client/src/components/PikadayWrap.js
+++ client/src/components/PikadayWrap.js
@@ -7,24 +7,27 @@ export default class PikadayWrap extends React.Component {
 		this.myRef = React.createRef()
 		// Min 18 years of age
 		this.maxDate = new Date(new Date().setFullYear(new Date().getFullYear() - 18))
+		// Get Max date from value props
+		this.maxDateValue =  new Date(this.props.value);
 		// Max year range
 		this.maxYear = new Date().getFullYear() - 18
 	}
 
 	componentDidMount () {
 		new Pikaday({
-		defaultDate: this.maxDate,
+		defaultDate: this.props.value !== undefined ? this.maxDateValue : this.maxDate,
 		field: this.myRef.current,
 		format: 'DD-MM-YYYY',
 		firstDay: 0,
 		maxDate: this.maxDate,
-		yearRange: [1900,this.maxYear]
+		yearRange: [1900,this.maxYear],
+		 position: "bottom right"
 		})
 	}
 
 	render () {
 		return	<div>
-					<input type='text' id="date" className='text-align-center' ref={this.myRef} autoComplete="off"/>
+					<input type='text' id={this.props.page === "profile" ? 'profile' : 'date'} className={this.props.page === "profile" ? 'text-align-center profile-pikaday' : 'text-align-center'} ref={this.myRef} autoComplete="off" placeholder={this.props.value}/>
 				</div>
 	}
 }
