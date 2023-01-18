115db5e1292de031be0708bb86a5eeddee685c3c
Cong Nguyen <kaom.n.92@gmail.com>
2023-01-18 14:33:13 +0200

    fixed eslint errors

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -90,7 +90,7 @@ export default function Profile() {
 			})();
 		}
 		return () => {mounted = false};
-	}, [params, socket, pathname]);
+	}, [params, socket, pathname, user.userid]);
 
 	useEffect(() => {
 		socket.on("online_response", data => {
